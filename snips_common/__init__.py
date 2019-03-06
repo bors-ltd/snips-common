98bdc4e62c88f94916a5132caa790e69dd7269c3
Bors, Ltd <github@bors-ltd.fr>
2019-03-06 23:15:51 +0100

    say a timedelta

--- snips_common/__init__.py
+++ snips_common/__init__.py
@@ -3,6 +3,8 @@ import datetime
 from pkg_resources import get_distribution
 import traceback
 
+from hermes_python.ontology.dialogue import DurationValue
+
 
 __version__ = get_distribution('snips-common').version
 
@@ -62,6 +64,31 @@ def french_duration(duration_slot):
     if not sentence:
         return ""
     return " ".join(sentence)
+
+
+def french_timedelta(delta):
+    """Express a Python timedelta as a sentence."""
+    seconds = int(delta.total_seconds())
+    duration_slot = DurationValue(0, 0, 0, 0, 0, 0, 0, 0, precision=1)
+
+    if seconds < 60:
+        # Only say the seconds under a minute
+        duration_slot.seconds = seconds
+    else:
+        duration_slot.minutes = seconds // 60
+        if duration_slot.minutes >= 60:
+            duration_slot.hours = duration_slot.minutes // 60
+            duration_slot.minutes %= 60
+        if duration_slot.hours >= 24:
+            duration_slot.days = duration_slot.hours // 24
+            duration_slot.hours %= 24
+        if duration_slot.days >= 7:
+            duration_slot.weeks = duration_slot.days // 7
+            duration_slot.days %= 7
+        # I won't go further, there is a reason why timedelta doesn't
+        # It would only make sense relative to a datetime
+
+    return french_duration(duration_slot)
 
 
 class ActionWrapper:
