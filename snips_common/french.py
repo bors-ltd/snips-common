1ba458c54be5fef95166392af4b29890d182b83a
Bors, Ltd <github@bors-ltd.fr>
2019-03-18 18:31:16 +0100

    rework implementations

--- snips_common/french.py
+++ snips_common/french.py
@@ -3,6 +3,7 @@ import datetime
 import babel.numbers
 from hermes_python.ontology.dialogue import DurationValue
 
+from .utils import timedelta_to_duration
 
 def french_number(number, digits=2):
     """Optimize the pronunciation of numbers for picoTTS."""
@@ -24,52 +25,28 @@ def french_number(number, digits=2):
 
 def french_duration(duration_slot):
     """Express the duration heard by Snips as a sentence."""
-    # TODO precision field?
+    # XXX precision field?
     sentence = []
-    for unit, word in [
-        ('years', "ans"),
-        ('quarters', "trimestres"),
-        ('months', "mois"),
-        ('weeks', "semaines"),
-        ('days', "jours"),
-        ('hours', "heures"),
-        ('minutes', "minutes"),
-        ('seconds', "secondes"),
+    for name, singular, plural in [
+        ('years', "un an", "{} ans"),
+        ('quarters', "un trimestre", "{} trimestres"),
+        ('months', "un mois", "{} mois"),
+        ('weeks', "une semaine", "{} semaines"),
+        ('days', "un jour", "{} jours"),
+        ('hours', "une heure", "{} heures"),
+        ('minutes', "une minute", "{} minutes"),
+        ('seconds', "une seconde", "{} secondes"),
     ]:
-        value = getattr(duration_slot, unit, None)
-        if value:
-            if value == 1 and word in ('semaines', 'heures', 'minutes', 'secondes'):
-                value = "une"
-            else:
-                value = str(value)
-            sentence.append(value)
-            sentence.append(word)
+        try:
+            value = getattr(duration_slot, name)
+        except AttributeError:
+            pass
+        else:
+            sentence.append(singular if value == 1 else plural.format(value))
 
-    if not sentence:
-        return ""
     return " ".join(sentence)
 
 
 def french_timedelta(delta):
     """Express a Python timedelta as a sentence."""
-    seconds = int(delta.total_seconds())
-    duration_slot = DurationValue(0, 0, 0, 0, 0, 0, 0, 0, precision=1)
-
-    if seconds < 60:
-        # Only say the seconds under a minute
-        duration_slot.seconds = seconds
-    else:
-        duration_slot.minutes = seconds // 60
-        if duration_slot.minutes >= 60:
-            duration_slot.hours = duration_slot.minutes // 60
-            duration_slot.minutes %= 60
-        if duration_slot.hours >= 24:
-            duration_slot.days = duration_slot.hours // 24
-            duration_slot.hours %= 24
-        if duration_slot.days >= 7:
-            duration_slot.weeks = duration_slot.days // 7
-            duration_slot.days %= 7
-        # I won't go further, there is a reason why timedelta doesn't
-        # It would only make sense relative to a datetime
-
-    return french_duration(duration_slot)
+    return french_duration(timedelta_to_duration(delta))
--- snips_common/utils.py
+++ snips_common/utils.py
@@ -2,12 +2,9 @@ import datetime
 
 
 def duration_to_timedelta(duration_slot):
-    """Convert a Snips duration slot to a Python timedelta object
-
-    Should already be in the Duration API!
-    """
+    """Convert a Snips duration slot to a Python timedelta object."""
     if duration_slot.years or duration_slot.quarters or duration_slot.months:
-        # I should find a higher-level package on PyPI
+        # Can't without a reference date
         raise NotImplementedError
 
     return datetime.timedelta(
@@ -19,3 +16,24 @@ def duration_to_timedelta(duration_slot):
     )
 
 
+def timedelta_to_duration(delta):
+    """Convert a Python timedelta object to a Snips duration slot."""
+    duration_slot = DurationValue(0, 0, 0, 0, 0, 0, 0, 0, precision=1)
+
+    duration_slot.seconds = int(delta.total_seconds())
+    if duration_slot.seconds >= 60:
+        duration_slot.minutes = duration_slot.seconds // 60
+        duration_slot.seconds %= 60
+    if duration_slot.minutes >= 60:
+        duration_slot.hours = duration_slot.minutes // 60
+        duration_slot.minutes %= 60
+    if duration_slot.hours >= 24:
+        duration_slot.days = duration_slot.hours // 24
+        duration_slot.hours %= 24
+    if duration_slot.days >= 7:
+        duration_slot.weeks = duration_slot.days // 7
+        duration_slot.days %= 7
+    # I won't go further, there is a reason why timedelta doesn't
+    # It would only make sense relative to a datetime
+
+    return duration_slot
