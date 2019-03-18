9c178c5eea76d9ca165ce09cdfe2b401a28d3ec7
Bors, Ltd <github@bors-ltd.fr>
2019-03-18 18:54:14 +0100

    write entrypoint only once

--- snips_common/actions.py
+++ snips_common/actions.py
@@ -1,14 +1,33 @@
 import traceback
 
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import MqttOptions
+
 from . import configs
+
+
+def message_for_this_site(config, intent_message):
+    site_id = config.get('secret', {}).get('site_id')
+    if site_id:
+        deliver_to = intent_message.site_id
+        if deliver_to != site_id:
+            print("This message is for", deliver_to, ", ignoring.")
+            return False
+    # We are the recipient or the app is not designed for multiroom
+    return True
 
 
 class ActionWrapper:
     """Common skeleton for actions"""
+    # The intent this action is subscribing to
+    intent = None
     # Class to load the configuration file
     config_parser = configs.SnipsConfigParser
     # Messages to send on the given errors
-    reactions = {}
+    reactions = {
+        # SomeError: "Sorry, I can't let you do that, Dave.",
+        # ...
+    }
 
     def __init__(self, hermes, intent_message, config):
         self.hermes = hermes
@@ -25,9 +44,9 @@ class ActionWrapper:
             "for site",
             intent_message.site_id
         )
-        config = cls.config_parser.read_configuration_file()
 
-        if not cls.message_for_this_site(config, intent_message):
+        config = cls.config_parser.read_configuration_file()
+        if not message_for_this_site(config, intent_message):
             return
 
         action_wrapper = cls(hermes, intent_message, config)
@@ -38,17 +57,6 @@ class ActionWrapper:
             action_wrapper.react_to_exception(exc)
         else:
             print("Action finished without error")
-
-    @staticmethod
-    def message_for_this_site(config, intent_message):
-        site_id = config.get('secret', {}).get('site_id')
-        if site_id:
-            deliver_to = intent_message.site_id
-            if deliver_to != site_id:
-                print("This message is for", deliver_to, ", ignoring.")
-                return False
-        # We are the recipient or the app is not designed for multiroom
-        return True
 
     def action(self):
         raise NotImplementedError
@@ -65,3 +73,14 @@ class ActionWrapper:
         message = message + " " + " ".join(str(arg) for arg in args)
         print('Ending session with message "%s"' % message)
         self.hermes.publish_end_session(current_session_id, message)
+
+    @classmethod
+    def main(cls):
+        if not cls.intent:
+            raise ValueError("Action %s is not bound to an intent" % cls)
+
+        mqtt_opts = MqttOptions()
+
+        with Hermes(mqtt_options=mqtt_opts) as hermes:
+            hermes.subscribe_intent(cls.intent, cls.callback)
+            hermes.start()
